fcba41761665c5d0827af2726718480a18b5019f
MarcMielle <marcmielle96@gmail.com>
2020-08-12 15:33:58 +0200

    Seeds working & validations on review

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Frites & Co', address: 'Paris', category: 'belgian', phone_number: '0123456789')
+Restaurant.create(name: 'Il Bocaccio', address: 'Nantes', category: 'italian', phone_number: '0123456788')
+Restaurant.create(name: 'La raclette', address: 'Meribel', category: 'french', phone_number: '0123456787')
+Restaurant.create(name: 'Japan Sakura', address: 'Lyon', category: 'japanese', phone_number: '0123456786')
+Restaurant.create(name: 'Le Bonheur', address: 'Saint-Cloud', category: 'chinese', phone_number: '0123456785')
